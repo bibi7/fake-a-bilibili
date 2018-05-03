1f43fa84f589c89c85df872989f4241c18464936
bibi <wzj2584454@126.com>
2018-05-03 22:35:39 +0800

    Dev: add middle-drawer info in javascript
    Signed-off-by: bibi <wzj2584454@126.com>

---
+++ bilibili/static/js/draw-recommend/newDrawer.js
@@ -0,0 +1,33 @@
+const newDrawer = {
+  drawerInfo: [
+    {
+      path: 'url(/static/img/middle-draw/first.jpg)',
+      smallPath: '',
+      title: '日常摸鱼',
+      userName: '好人小仙子',
+    },
+    {
+      path: 'url(/static/img/middle-draw/second.jpg)',
+      smallPath: '',
+      title: '直播看小男孩打♂架',
+      userName: '1',
+    },{
+      path: 'url(/static/img/middle-draw/third.jpg)',
+      smallPath: '',
+      title: 'fafa',
+      userName: '1',
+    },{
+      path: 'url(/static/img/middle-draw/forth.jpg)',
+      smallPath: '',
+      title: '胡乱画画茶',
+      userName: '1',
+    },{
+      path: 'url(/static/img/middle-draw/fifth.jpg)',
+      smallPath: '',
+      title: '日常，回家后',
+      userName: '1',
+    },
+  ]
+}
+
+export {newDrawer}
